796802e8ba56cce6f04710874dd55086e6bbde66
Erik <eln17011@student.mdh.se>
2021-11-05 12:36:57 +0100

    Added micro-ros as a component
    Added a component which should add micro-ros functionality

--- GPS/main/main.c
+++ GPS/main/main.c
@@ -1,58 +1,59 @@
-#include "nmea.h"
-#include "protocol.h"
 #include "main.h"
 
-int app_main(void)
-{
-    
-    //variables
+#include <rcl/rcl.h>
+
+#include "nmea.h"
+#include "protocol.h"
+
+int app_main(void) {
+    // variables
     int i = 0;
     int c = 0;
 
     float lon = 0;
     float lat = 0;
 
-    uint8_t reg_addr = 0xFF; //output register
+    uint8_t reg_addr = 0xFF;  // output register
 
-    uint8_t * data = calloc(100,4);
-    char * msg = calloc(100,1);
+    uint8_t* data = calloc(100, 4);
+    char* msg = calloc(100, 1);
 
-    if(msg == NULL){
+    if (msg == NULL) {
         printf("Calloc for msg failed\n");
     }
-    if(data == NULL){
+    if (data == NULL) {
         printf("Calloc for data failed\n");
     }
 
-    //configure i2c
+    // configure i2c
     configure_i2c_master();
+    i2c_ack_type_t ack = 0;
+    while (1) {
+        // read data from the sensor
+        i2c_master_write_read_device(I2C_MASTER_NUM, SLAVE_ADDR, &reg_addr, 1, data, 400, I2C_MASTER_TIMEOUT_MS /
+        // portTICK_RATE_MS);
 
-    while(1){
-        //read data from the sensor
-        i2c_master_write_read_device(I2C_MASTER_NUM, SLAVE_ADDR, &reg_addr, 1, data, 400, I2C_MASTER_TIMEOUT_MS/portTICK_RATE_MS);
-
-        //convert the data to char
+        // convert the data to char
         i = 0;
 
         lon = 0;
         lat = 0;
-        //Only convert up to the * sign, since that marks the end of a message
-        while((data[i] != 42) && (i < 100)){
+        // Only convert up to the * sign, since that marks the end of a message
+        while ((data[i] != 42) && (i < 100)) {
             msg[i] = (char)data[i];
             i++;
         }
 
-        //get the GPS position
-        if(!getPos(msg, &lat, &lon)){
+        // get the GPS position
+        if (!getPos(msg, &lat, &lon)) {
             c++;
-            printf("No data avalible for %d readings\n", c);          
-        }
-        else{
+            printf("No data avalible for %d readings\n", c);
+        } else {
             c = 0;
             printf("Lat: %.4f : Long: %.4f\n", lat, lon);
         }
-        
-        //delay for easier to read prints
+
+        // delay for easier to read prints
         vTaskDelay(50);
     }
     return 0;
