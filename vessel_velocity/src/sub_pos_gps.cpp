ae71c0a918d0fe42ea71f3e1faeca9347b8e7604
Peter Nguyen <pnn16004@student.mdh.se>
2021-11-12 09:55:33 +0100

    Enklare att använda

--- vessel_velocity/src/sub_pos_gps.cpp
+++ vessel_velocity/src/sub_pos_gps.cpp
@@ -20,9 +20,9 @@ public:
   : Node("pubsub")
   { //           node  create publisher of type String      Topic name and value
     GPS_sub_ = this->create_subscription<std_msgs::msg::Float32MultiArray>( //Constructor uses the node's create_subscription class for callbacks
-      "/position/IMU", 50, std::bind(&MinimalPubSub::GPS_topic_callback, this, _1)); //No timer, instant response
+      "/position/GPS", 50, std::bind(&MinimalPubSub::GPS_topic_callback, this, _1)); //No timer, instant response
 
-    publisher_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("/position/pospub", 50); //Init msg type, topic name and msg size
+    publisher_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("/velocity/calculator", 50); //Init msg type, topic name and msg size
 
     nodeTime_ = this->get_clock();
   }
@@ -30,12 +30,14 @@ public:
 private:
   void GPS_topic_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg) //Receives the string message data published over the topic
   {
-    auto message = msg->data[0]; //Test
+    //auto message = msg->data[0]; //Test
+    auto message = std_msgs::msg::Float32MultiArray();
+    
+    RCLCPP_INFO(this->get_logger(), "Lat: '%lf', Lon: '%lf'", msg->data[0], msg->data[1]); //Write to console using macro
+    //RCLCPP_INFO(this->get_logger(), "Yaw: '%lf, Pitch: '%lf, Roll: '%lf, '", msg->data[0], msg->data[1], msg->data[2]);
 
     auto curr_GPS_ = msg->data; //Get current GPS reading
     currTime_ = nodeTime_->now(); //Get time elapsed since node initialization
-
-    //RCLCPP_INFO(this->get_logger(), "I heard: '%lf'", curr_GPS_->data[0]); //Write to console using macro
 
     if (prev_GPS_.capacity() != 0) { //Atleast two GPS readings
 
@@ -63,6 +65,9 @@ private:
     float dot = (currX * prevX + currY * prevY + currZ * prevZ);
     float cos_theta = dot / (r * r);
 
+    if (cos_theta > 1) //acos of val > 1 = nan
+      cos_theta = 1;
+
     float theta = acos(cos_theta);
 
     float dist = r * theta;
@@ -73,7 +78,8 @@ private:
     float velocity = dist / avgTime; //m/s
     //velocity = (velocity * 3600.0) / 1000.0; //km/h
 
-    message = velocity;
+    //RCLCPP_INFO(this->get_logger(), "currLat: %f, prevLon: %f, currY: %f, prevY: %f, dot: %f, cos_theta: %f, theta: %f, dist: %f, avgTime: %f", currLat, prevLon, currY, prevY, dot, cos_theta, theta, dist, avgTime);
+    message.data = {velocity};
     RCLCPP_INFO(this->get_logger(), "velocity: '%f'", velocity);
     }
 
@@ -81,6 +87,8 @@ private:
     prev_GPS_ = curr_GPS_;
     prevTime_ = currTime_;
 
+    //while (publisher_->get_subscription_count() < 1);
+    rclcpp::sleep_for(std::chrono::nanoseconds(5)); //To have enough time to publish
     publisher_->publish(message);
   }
 
