8638eaa2f4c86095fadc7c60e03ee22bd2be1fdf
Erik <eln17011@student.mdh.se>
2021-11-19 09:54:56 +0100

    angle2Goal
    Added so it computes the correct angle to goal

--- ROS2/rudder_path/src/rudder_path_pid.cpp
+++ ROS2/rudder_path/src/rudder_path_pid.cpp
@@ -20,7 +20,7 @@
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
-float currAngle = 0;
+float Angle2Goal = 0;
 int c = 0;
 float earth_rad = 6371000;
 
@@ -39,27 +39,43 @@ class MinimalSubPub : public rclcpp::Node {
    private:
     void topic_callback(const STD_MULTIFLOAT::SharedPtr msg) {
         auto message = STD_FLOAT();
-        // Sets the message that was read from the subscriber
-        // float roll = msg->data[2];
-        // float pitch = msg->data[1];
+
         float yaw = msg->data[0];
+        float heading = 0;
 
-        float lat = 59.370687 * M_PIl / 180.0;
-        float lon = 16.336636 * M_PIl / 180.0;
-        float glat = 60.000000 * M_PIl / 180.0;
-        float glon = 16.000000 * M_PIl / 180.0;
+        float curr_lat = 59.370687 * M_PIl / 180.0;
+        float curr_lon = 16.336636 * M_PIl / 180.0;
+        float goal_lat = 60.000000 * M_PIl / 180.0;
+        float goal_lon = 16.000000 * M_PIl / 180.0;
 
-        float u = sin((glat - lat) / 2);
-        float v = sin((glon - lon) / 2);
-        float dDist = 2.0 * earth_rad * asin(sqrt(u * u + cos(lat) * cos(glat) * v * v));
+        float u = sin((goal_lat - curr_lat) / 2);
+        float v = sin((goal_lon - curr_lon) / 2);
+        float dDist = 2.0 * earth_rad * asin(sqrt(u * u + cos(curr_lat) * cos(goal_lat) * v * v));
 
         if (yaw < 0) {
             yaw = 180 + (180 + yaw);
         }
 
-        currAngle = yaw;
-        message.data = currAngle;
-        printf("[%d] [Boat Angle: %.2f] [Distance to goal: %f]\n", c++, currAngle, dDist);
+        heading = yaw;
+
+        // Compute initial bearing between true north and the distance vector between goal pos and current pos
+        float bearing = atan2(sin(goal_lon - curr_lon) * cos(goal_lat),
+                              cos(curr_lat) * sin(goal_lat) - sin(curr_lat) * cos(goal_lat) * cos(goal_lon - curr_lon));
+
+        // Convert rad to degrees, and add 360 degrees to normalize it. Then perform mod 360 to not get a
+        // degree above 360.
+        bearing = ((bearing * 180) / M_PIl) + 360;
+        bearing = fmod(bearing, 360);
+
+        // Compute angle to goal and mod it with 360.
+        Angle2Goal = bearing + heading;
+        Angle2Goal = fmod(Angle2Goal, 360);
+
+        printf("[%d] [Boat Angle: %.2f] [Distance to goal: %.2f] [Angle to goal is: %.2f] [Final bearing: %.2f]\n", c++,
+               heading, dDist, Angle2Goal, bearing);
+
+        // Assign data to the message and publish it
+        message.data = Angle2Goal;
         currTime_ = nodeTime_->now();
         rclcpp::sleep_for(std::chrono::nanoseconds(1));
         publisher_->publish(message);
