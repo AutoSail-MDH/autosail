5b59538cd3ccc5c43fded26141cc9b1950d957b1
Erik <eln17011@student.mdh.se>
2021-11-22 15:10:16 +0100

    multi sub
    fully working multi sub added

--- ROS2/rudder_path/src/rudder_path_pid.cpp
+++ ROS2/rudder_path/src/rudder_path_pid.cpp
@@ -11,7 +11,9 @@
 #include "std_msgs/msg/string.hpp"
 
 // Change these to your topics
-#define SUB_TOPIC "/position/IMU"
+#define SUB_TOPIC_1 "/position/IMU"
+#define SUB_TOPIC_2 "/position/GPS"
+#define SUB_TOPIC_3 "/position/GOAL"
 #define PUB_TOPIC "/rudder/ANGLE"
 // Change this to your message type, made this define to not have to write the long expression
 #define STD_MULTIFLOAT std_msgs::msg::Float32MultiArray
@@ -19,39 +21,58 @@
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
+using std::placeholders::_2;
+
+float c_lat = 0.0;
+float c_long = 0.0;
+float g_lat = 0.0;
+float g_long = 0.0;
+
+float yaw = 0.0;
 
 float Angle2Goal = 0;
 int c = 0;
-float earth_rad = 6371000;
 
 class MinimalSubPub : public rclcpp::Node {
    public:
     MinimalSubPub() : Node("subpub") {
-        subscriber_ = this->create_subscription<STD_MULTIFLOAT>(
+        subscriber_IMU = this->create_subscription<STD_MULTIFLOAT>(
 
-            SUB_TOPIC, 50, std::bind(&MinimalSubPub::topic_callback, this, _1));
+            SUB_TOPIC_1, 50, std::bind(&MinimalSubPub::IMU_callback, this, _1));
+        subscriber_GPS = this->create_subscription<STD_MULTIFLOAT>(
+
+            SUB_TOPIC_2, 50, std::bind(&MinimalSubPub::GPS_callback, this, _1));
+        subscriber_GOAL = this->create_subscription<STD_MULTIFLOAT>(
+
+            SUB_TOPIC_3, 50, std::bind(&MinimalSubPub::GOAL_callback, this, _1));
 
         publisher_ = this->create_publisher<STD_FLOAT>(PUB_TOPIC, 50);
 
         nodeTime_ = this->get_clock();  // Create clock starting at the time of node creation
+        timer_ = this->create_wall_timer(100ms, std::bind(&MinimalSubPub::topic_callback, this));
     }
 
    private:
-    void topic_callback(const STD_MULTIFLOAT::SharedPtr msg) {
+    void IMU_callback(const STD_MULTIFLOAT::SharedPtr msg) { yaw = msg->data[0]; }
+
+    void GPS_callback(const STD_MULTIFLOAT::SharedPtr msg) {
+        c_lat = msg->data[0] * M_PIl / 180.0;
+        c_long = msg->data[1] * M_PIl / 180.0;
+    }
+
+    void GOAL_callback(const STD_MULTIFLOAT::SharedPtr msg) {
+        g_lat = msg->data[0] * M_PIl / 180.0;
+        g_long = msg->data[1] * M_PIl / 180.0;
+    }
+
+    void topic_callback() {
         auto message = STD_FLOAT();
 
-        float yaw = msg->data[0];
         float heading = 0;
 
-        float curr_lat = 59.370687 * M_PIl / 180.0;
-        float curr_lon = 16.336636 * M_PIl / 180.0;
-        float goal_lat = 60.000000 * M_PIl / 180.0;
-        float goal_lon = 16.000000 * M_PIl / 180.0;
-
-        // if (Angle2Goal > 1) {
-        float u = sin((goal_lat - curr_lat) / 2);
-        float v = sin((goal_lon - curr_lon) / 2);
-        float dDist = 2.0 * earth_rad * asin(sqrt(u * u + cos(curr_lat) * cos(goal_lat) * v * v));
+        float u = sin((g_lat - c_lat) / 2);
+        float v = sin((g_long - c_long) / 2);
+        float dDist = 2.0 * earth_rad * asin(sqrt(u * u + cos(c_lat) * cos(g_lat) * v * v));
 
         if (yaw < 0) {
             yaw = 180 + (180 + yaw);
@@ -60,8 +81,8 @@ class MinimalSubPub : public rclcpp::Node {
         heading = yaw;
 
         // Compute initial bearing between true north and the distance vector between goal pos and current pos
-        float bearing = atan2(sin(goal_lon - curr_lon) * cos(goal_lat),
-                              cos(curr_lat) * sin(goal_lat) - sin(curr_lat) * cos(goal_lat) * cos(goal_lon - curr_lon));
+        float bearing = atan2(sin(g_long - c_long) * cos(g_lat),
+                              cos(c_lat) * sin(g_lat) - sin(c_lat) * cos(g_lat) * cos(g_long - c_long));
 
         // Convert rad to degrees, and add 360 degrees to normalize it. Then perform mod 360 to not get a
         // degree above 360.
@@ -78,7 +99,6 @@ class MinimalSubPub : public rclcpp::Node {
             c++, heading, dDist, Angle2Goal, bearing);
 
         // Assign data to the message and publish it
-        //}
 
         message.data = Angle2Goal;
         currTime_ = nodeTime_->now();
@@ -86,38 +106,20 @@ class MinimalSubPub : public rclcpp::Node {
         publisher_->publish(message);
     }
     // Defines
-    rclcpp::Subscription<STD_MULTIFLOAT>::SharedPtr subscriber_;
+    rclcpp::Subscription<STD_MULTIFLOAT>::SharedPtr subscriber_IMU;
+    rclcpp::Subscription<STD_MULTIFLOAT>::SharedPtr subscriber_GPS;
+    rclcpp::Subscription<STD_MULTIFLOAT>::SharedPtr subscriber_GOAL;
     rclcpp::Publisher<STD_FLOAT>::SharedPtr publisher_;
     rclcpp::Clock::SharedPtr nodeTime_;
     rclcpp::Time currTime_;
     rclcpp::Time prevTime_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    float earth_rad = 6371000;
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<MinimalSubPub>());
     rclcpp::shutdown();
     return 0;
 }
-
-class MultiSubscriber : public rclcpp::Node {
-   public:
-    message_filters::Subscriber<sensor_msgs::msg::Image> image_sub_;
-    message_filters::Subscriber<sensor_msgs::msg::Image> disparity_sub_;
-    MultiSubscriber(const std::string& name) : Node(name) {
-        image_sub_.subscribe(this, "/image_color");
-        disparity_sub_.subscribe(this, "/image_disparity");
-        typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::msg::Image, sensor_msgs::msg::Image>
-            approximate_policy;
-        message_filters::Synchronizer<approximate_policy> syncApproximate(approximate_policy(10), image_sub_,
-                                                                          disparity_sub_);
-        syncApproximate.setMaxIntervalDuration(rclcpp::Duration(3, 0));  // Added after a comment
-        syncApproximate.registerCallback(&MultiSubscriber::disparityCb, this);
-    }
-
-   private:
-    void disparityCb(const sensor_msgs::msg::Image::SharedPtr disparity_msg,
-                     const sensor_msgs::msg::Image::SharedPtr color_msg) {
-        std::cout << "Hello messages are being received";
-    }
-};
