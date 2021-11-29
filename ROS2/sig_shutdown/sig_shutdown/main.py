72e8fc0f3a1906d09a25575183ed9d3dec885a88
Emil Persson <taza@live.se>
2021-11-29 10:12:29 +0100

    Added doc

--- ROS2/sig_shutdown/sig_shutdown/main.py
+++ ROS2/sig_shutdown/sig_shutdown/main.py
@@ -2,17 +2,21 @@ import rclpy
 from rclpy.node import Node
 
 class MinimalPublisher(Node):
+    """Minimal publisher that sends FATAL error messages to the console, resulting in a shutdown of boat"""
 
     def __init__(self):
         super().__init__('minimal_publisher')
+        # .fatal() will result in error msg 50
         self.get_logger().fatal('Shutting down')
-    
-def main(args=None):
+
+
+def main(args=None):s
     rclpy.init(args=args)
     minimal_publisher = MinimalPublisher()
     rclpy.spin(minimal_publisher)
     minimal_publisher.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
