f980f63614f6875cad82879f879fba1babf09feb
fak18001 <fak18001.student.mdu.se>
2022-10-24 13:25:03 +0200

    Add custom message for velocity calculation

--- src/pkgs/vessel_velocity/src/velocity_calculation.cpp
+++ src/pkgs/vessel_velocity/src/velocity_calculation.cpp
@@ -4,66 +4,73 @@
 #include <chrono>
 #include <cstring>
 #include <memory>
+
 // Dependencies
 #include <std_msgs/msg/float32_multi_array.hpp>  //Built-in message types for publishing data
+#include <autosail_message/msg/velocity_calculation_message.hpp>
+#include <autosail_message/msg/gnss_message.hpp>
 
 #include "rclcpp/rclcpp.hpp"  //Common for ROS 2
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
+
+#define VELOCITY_MSG autosail_message::msg::VelocityCalculationMessage
+#define GNSS_MSG autosail_message::msg::GNSSMessage
+
 class VelocityCalculation : public rclcpp::Node  // Create node class by inheriting
 {                                          // this refers to the MinimalPublisher node
    public:
     VelocityCalculation()  // The public constructor intits node
         : Node("velocity_node") {
-        GPS_sub_ =
-            this->create_subscription<std_msgs::msg::Float32MultiArray>(  // Constructor uses the node's
+        subscriber_POSE =
+            this->create_subscription<GNSS_MSG>(  // Constructor uses the node's
                                                                           // create_subscription class for callbacks
                 "/position/GPS", 50, // "/sensor/gnss"
                 std::bind(&VelocityCalculation::gnss_topic_callback, this, _1));  // No timer, instant response
 
-        publisher_ = this->create_publisher<std_msgs::msg::Float32MultiArray>(
+        publisher_ = this->create_publisher<VELOCITY_MSG>(
             "/sensor/velocity", 50);  // Init msg type, topic name and msg size
 
         nodeTime_ = this->get_clock();  // Create clock starting at the time of node creation
     }
 
    private:
-    void gnss_topic_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg) {
+    void gnss_topic_callback(const GNSS_MSG::SharedPtr msg) {
         auto message = std_msgs::msg::Float32MultiArray();
 
-        auto curr_GPS_ = msg->data;    // Get current GPS reading
-        currTime_ = nodeTime_->now();  // Get time elapsed since node initialization
+        auto current_gnss_ = msg->data;    // Get current GPS reading
+        current_time_ = nodeTime_->now();  // Get time elapsed since node initialization
 
-        if (prev_GPS_.capacity() != 0) {  // Atleast two GPS readings
+        if (previous_gnss_.capacity() != 0) {  // Atleast two GPS readings
 
             // Get the distance between current and previous GPS reading
 
             // Convert coordinates from degrees to radians
-            float currLat = curr_GPS_[0] * M_PIl / 180.0;
-            float currLon = curr_GPS_[1] * M_PIl / 180.0;
+            float current_lat = current_gnss_[0] * M_PIl / 180.0;
+            float current_lon = current_gnss_[1] * M_PIl / 180.0;
 
-            float prevLat = prev_GPS_[0] * M_PIl / 180.0;
-            float prevLon = prev_GPS_[1] * M_PIl / 180.0;
+            float previous_lat = previous_gnss_[0] * M_PIl / 180.0;
+            float previous_lon = previous_gnss_[1] * M_PIl / 180.0;
 
             // Haversine formula: distance between two points on earth
-            float u = sin((currLat - prevLat) / 2);
-            float v = sin((currLon - prevLon) / 2);
-            float dist = 2.0 * r * asin(sqrt(u * u + cos(prevLat) * cos(currLat) * v * v));
+            float u = sin((current_lat - previous_lat) / 2);
+            float v = sin((current_lon - previous_lon) / 2);
+            float dist = 2.0 * r * asin(sqrt(u * u + cos(previous_lat) * cos(current_lat) * v * v));
 
             // Calculate average speed
 
-            float deltaTime = abs((float)((currTime_ - prevTime_).nanoseconds()) /
+            float deltaTime = abs((float)((current_time_ - previous_time_).nanoseconds()) /
                                   pow(10, 9));  // Time difference in seconds, with nanosecond precision
             float velocity = dist / deltaTime;  // m/s
 
-            message.data = {velocity, currLat, currLon, deltaTime};  // Data to publish
+            message.data = {velocity, current_lat, current_lon, deltaTime};  // Data to publish
 
-            if (curr_GPS_[0] != prev_GPS_[0] ||
-                curr_GPS_[1] != prev_GPS_[1]) {  // Print only if position changed since last
+            if (current_gnss_[0] != previous_gnss_[0] ||
+                current_gnss_[1] != previous_gnss_[1]) {  // Print only if position changed since last
                 RCLCPP_INFO(this->get_logger(), "velocity: '%f'", velocity);
-                RCLCPP_INFO(this->get_logger(), "Lat: '%lf', Lon: '%lf'", curr_GPS_[0], curr_GPS_[1]);
+                RCLCPP_INFO(this->get_logger(), "Lat: '%lf', Lon: '%lf'", current_gnss_[0], current_gnss_[1]);
                 RCLCPP_INFO(this->get_logger(), "deltaTime: '%f'", (float)deltaTime);
                 RCLCPP_INFO(this->get_logger(), "distance: '%f'", (float)dist);
 
@@ -71,20 +78,20 @@ class VelocityCalculation : public rclcpp::Node  // Create node class by inherit
                 publisher_->publish(message);
             }
         } else  // Print start position
-            RCLCPP_INFO(this->get_logger(), "Lat: '%lf', Lon: '%lf'", curr_GPS_[0], curr_GPS_[1]);
+            RCLCPP_INFO(this->get_logger(), "Lat: '%lf', Lon: '%lf'", current_gnss_[0], current_gnss_[1]);
 
         // Save current GPS readings
-        prev_GPS_ = curr_GPS_;
-        prevTime_ = currTime_;
+        previous_gnss_ = current_gnss_;
+        previous_time_ = current_time_;
     }
 
     // Declaration of fields
-    rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr GPS_sub_;
-    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr publisher_;
+    rclcpp::Subscription<GNSS_MSG>::SharedPtr subscriber_POSE;
+    rclcpp::Publisher<VELOCITY_MSG>::SharedPtr publisher_;
     rclcpp::Clock::SharedPtr nodeTime_;
-    std::vector<float> prev_GPS_;
-    rclcpp::Time currTime_;
-    rclcpp::Time prevTime_;
+    std::vector<float> previous_gnss_;
+    rclcpp::Time current_time_;
+    rclcpp::Time previous_time_;
     float r = 6371000;  // Earth radius in meters
 };
 
