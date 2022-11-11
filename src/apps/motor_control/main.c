c418cb5a4a55850f527e3f5703141e6e0a775306
Karl <ksd18001@student.mdu.se>
2022-11-11 09:44:34 +0100

    Add pwm control in rudder_callback, not working!

--- src/apps/motor_control/main.c
+++ src/apps/motor_control/main.c
@@ -4,8 +4,8 @@
 #include <rclc/rclc.h>
 #include <rmw_microros/rmw_microros.h>
 #include <rmw_microxrcedds_c/config.h>
-#include <autosail_message/msg/sail_angle_message.h>
-#include <autosail_message/msg/sail_furl_message.h>
+//#include <autosail_message/msg/sail_angle_message.h>
+//#include <autosail_message/msg/sail_furl_message.h>
 #include <autosail_message/msg/rudder_control_message.h>
 #include <stdio.h>
 #include <sys/time.h>
@@ -39,20 +39,21 @@
 
 // You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000
 // microseconds
-#define SERVO_MIN_PULSEWIDTH_US (800)   // Minimum pulse width in microsecond
-#define SERVO_MAX_PULSEWIDTH_US (2200)  // Maximum pulse width in microsecond
-#define SERVO_MAX_DEGREE (90)           // Maximum angle in degree upto which servo can rotate
-#define SERVO_PULSE_GPIO_SAIL (19)      // GPIO connects to the PWM signal line
-#define SERVO_PULSE_GPIO_RUDDER (18)    // GPIO connects to the PWM signal line
+#define SERVO_MIN_PULSEWIDTH_US 800     // Minimum pulse width in microsecond
+#define SERVO_MAX_PULSEWIDTH_US 2200    // Maximum pulse width in microsecond
+#define SERVO_MAX_DEGREE 90             // Maximum angle in degree upto which servo can rotate
+#define SERVO_MIN_DEGREE 0            // Minimum angle
+#define SERVO_PULSE_GPIO_SAIL 19        // GPIO connects to the PWM signal line
+#define SERVO_PULSE_GPIO_RUDDER 18      // GPIO connects to the PWM signal line
 
 #define MS_DELAY (10)  // Callback delay
 
 rcl_subscription_t sub_sail;
 rcl_subscription_t sub_rudder;
 rcl_subscription_t sub_furl;
-autosail_message__msg__SailAngleMessage angle_msg;
+//autosail_message__msg__SailAngleMessage angle_msg;
 autosail_message__msg__RudderControlMessage rudder_msg;
-autosail_message__msg__SailFurlMessage furl_msg;
+//autosail_message__msg__SailFurlMessage furl_msg;
 
 struct timeval startTime;
 struct timeval currTime;
@@ -60,6 +61,7 @@ struct timeval prevTimeSail;
 struct timeval prevTimeRudder;
 struct timeval prevTimeFurl;
 
+/*
 void sail_callback(const void *msgin) {
     const autosail_message__msg__SailAngleMessage *msg = (const autosail_message__msg__SailAngleMessage *)msgin;
 
@@ -80,6 +82,7 @@ void sail_callback(const void *msgin) {
 
     gettimeofday(&prevTimeSail, NULL);
 }
+*/
 
 void rudder_callback(const void *msgin) {
     const autosail_message__msg__RudderControlMessage *msg = (const autosail_message__msg__RudderControlMessage *)msgin;
@@ -88,23 +91,39 @@ void rudder_callback(const void *msgin) {
 
     int32_t angle = msg->rudder_angle;  // Data sent in degrees
     printf("Rudder angle received: %d\r\n", angle);
+    /*
+    if (angle > 45) {
+        angle = 45;
+    }
+    else if (angle < -45){
+        angle = 45;
+    }
+    */
 
+    
+    uint32_t duty_us = ((angle - SERVO_MIN_DEGREE) * (SERVO_MAX_PULSEWIDTH_US - SERVO_MIN_PULSEWIDTH_US) / (SERVO_MAX_DEGREE - SERVO_MIN_DEGREE)) + SERVO_MIN_PULSEWIDTH_US;
+    
+    /*
     uint32_t duty_us =
         (angle + SERVO_MAX_DEGREE) * (SERVO_MAX_PULSEWIDTH_US - SERVO_MIN_PULSEWIDTH_US) / (2 * SERVO_MAX_DEGREE) +
         SERVO_MIN_PULSEWIDTH_US;
+    */
 
     ESP_ERROR_CHECK(mcpwm_set_duty_in_us(MCPWM_UNIT_1, MCPWM_TIMER_1, MCPWM_OPR_A, duty_us));
     vTaskDelay(pdMS_TO_TICKS(MS_DELAY));  // Add delay, since it takes time for servo to rotate, generally
                                           // 100ms/60degree rotation under 5V power supply
 
     printf("Rudder duty cycle set to: %f\r\n", (float)duty_us);
+    
 
     gettimeofday(&prevTimeRudder, NULL);
 }
 
+/*
 void furl_callback(const void *msgin) {
     gettimeofday(&prevTimeFurl, NULL);
 }
+*/
 
 void appMain(void* arg) {
 
@@ -125,22 +144,27 @@ void appMain(void* arg) {
     rcl_node_t node;
     RCCHECK(rclc_node_init_default(&node, "motor_node", "", &support));
 
+    /*
     RCCHECK(rclc_subscription_init_default(&sub_sail, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(autosail_message, msg, SailAngleMessage),
                                            "/actuator/sail"));
+    */
     vTaskDelay(500 / portTICK_PERIOD_MS);
     RCCHECK(rclc_subscription_init_default(&sub_rudder, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(autosail_message, msg, RudderControlMessage),
                                            "/actuator/rudder"));
+    
+    /*
     vTaskDelay(500 / portTICK_PERIOD_MS);
     RCCHECK(rclc_subscription_init_default(&sub_furl, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(autosail_message, msg, SailFurlMessage),
                                            "/actuator/furl"));
+    */
 
     // create executor
     rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
     RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
 
-    RCCHECK(rclc_executor_add_subscription(&executor, &sub_sail, &angle_msg, &sail_callback, ON_NEW_DATA));
+    //RCCHECK(rclc_executor_add_subscription(&executor, &sub_sail, &angle_msg, &sail_callback, ON_NEW_DATA));
     RCCHECK(rclc_executor_add_subscription(&executor, &sub_rudder, &rudder_msg, &rudder_callback, ON_NEW_DATA));
-    RCCHECK(rclc_executor_add_subscription(&executor, &sub_furl, &furl_msg, &furl_callback, ON_NEW_DATA));
+    //RCCHECK(rclc_executor_add_subscription(&executor, &sub_furl, &furl_msg, &furl_callback, ON_NEW_DATA));
 
     // configure motor control pulse width modulator (MCPWM)
     mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A,
